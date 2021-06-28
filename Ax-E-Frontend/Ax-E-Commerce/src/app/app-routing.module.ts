1384a509d4654a095a6d0a6d5da6e82482c85b70
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-28 07:58:50 +0530

    Auth module create

--- Ax-E-Frontend/Ax-E-Commerce/src/app/app-routing.module.ts
+++ Ax-E-Frontend/Ax-E-Commerce/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'Auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
