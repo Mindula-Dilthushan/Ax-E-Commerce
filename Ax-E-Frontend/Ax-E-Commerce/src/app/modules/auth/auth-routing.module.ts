167a87f948e0d944994cf339e8514c289de882e2
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-28 08:18:25 +0530

    login page redirect to

--- Ax-E-Frontend/Ax-E-Commerce/src/app/modules/auth/auth-routing.module.ts
+++ Ax-E-Frontend/Ax-E-Commerce/src/app/modules/auth/auth-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import {LoginPageComponent} from "./components/login-page/login-page.component";
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/login',pathMatch:'full' },
+  { path: 'login', component: LoginPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
