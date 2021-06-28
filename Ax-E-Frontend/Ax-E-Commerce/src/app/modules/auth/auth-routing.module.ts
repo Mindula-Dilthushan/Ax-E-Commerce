abd46d55b546d0d5164182d71bea51f1e5397dbb
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-28 11:49:00 +0530

    auth components changing

--- Ax-E-Frontend/Ax-E-Commerce/src/app/modules/auth/auth-routing.module.ts
+++ Ax-E-Frontend/Ax-E-Commerce/src/app/modules/auth/auth-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginPageComponent} from "./components/login-page/login-page.component";
+import {SignUpItemsPageComponent} from "./components/login-page/inner-items/sign-up-items-page/sign-up-items-page.component";
+import {LoginItemsPageComponent} from "./components/login-page/inner-items/login-items-page/login-items-page.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth/login',pathMatch:'full' },
-  { path: 'login', component: LoginPageComponent}
+  { path: 'login', component: LoginPageComponent,
+    children : [
+      {
+        path : 'register', component : SignUpItemsPageComponent
+      },
+      {
+        path : 'access', component : LoginItemsPageComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
